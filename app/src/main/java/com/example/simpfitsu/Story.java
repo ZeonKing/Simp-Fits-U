17c9780bb614e06323120f4c4b365b82b75c2afe
ZeonKing <jaritanto@yahoo.co.id>
2020-11-07 11:16:20 -0800

    2nd commit

---
+++ app/src/main/java/com/example/simpfitsu/Story.java
@@ -0,0 +1,12 @@
+package com.example.simpfitsu;
+
+public class Story {
+    private String story;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+
+    public Story(String story, String option1,String option2,String option3,String option4) {}
+
+}
---
+++ app/src/main/java/com/example/simpfitsu/storyManager.java
@@ -0,0 +1,5 @@
+package com.example.simpfitsu;
+
+public class storyManager implements Iterable<story> {
+    private
+}
